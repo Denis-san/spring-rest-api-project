1a6d13ef46a7de1a6805011310cc9780d33e7c9d
Denis-san <denilson.nt.nc@gmail.com>
2022-08-20 17:38:53 -0300

    created project

---
+++ src/main/java/com/dsan/springrestapicourse/SpringRestApiCourseApplication.java
@@ -0,0 +1,13 @@
+package com.dsan.springrestapicourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApiCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestApiCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dsan/springrestapicourse/SpringRestApiCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsan.springrestapicourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApiCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
