86d07420b812bc4afd128b3cc3eb11f2da593ecc
Denis-san <denilson.nt.nc@gmail.com>
2022-08-20 18:59:53 -0300

    creating instantiation operation

--- src/main/java/com/dsan/springrestapicourse/SpringRestApiCourseApplication.java
+++ src/main/java/com/dsan/springrestapicourse/SpringRestApiCourseApplication.java
@@ -1,13 +1,33 @@
 package com.dsan.springrestapicourse;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.dsan.springrestapicourse.domain.Category;
+import com.dsan.springrestapicourse.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class SpringRestApiCourseApplication {
+public class SpringRestApiCourseApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRestApiCourseApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Category cat = new Category(null, "Computing");
+		Category cat2 = new Category(null, "Office");
+
+		categoryRepository.saveAll(Arrays.asList(cat, cat2));
+
 	}
 
 }
