11bce5f24ce6177fafc2f8f8d6d0a7f891e24396
Denis-san <denilson.nt.nc@gmail.com>
2022-08-20 18:03:20 -0300

    first domain class testing

--- src/main/java/com/dsan/springrestapicourse/resources/CategoryResource.java
+++ src/main/java/com/dsan/springrestapicourse/resources/CategoryResource.java
@@ -1,16 +1,30 @@
 package com.dsan.springrestapicourse.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dsan.springrestapicourse.domain.Category;
+
 @RestController
 @RequestMapping("/categories")
 public class CategoryResource {
-	
+
 	@GetMapping("/list")
-	public String list() {
-		return "Rest testing!";
+	public List<Category> list() {
+
+		Category cat = new Category(1, "Computing");
+		Category cat2 = new Category(2, "Office");
+
+		List<Category> list = new ArrayList<Category>();
+
+		list.add(cat);
+		list.add(cat2);
+
+		return list;
 	}
 
 }
