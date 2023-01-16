c7ee248692c508e5b3d9ab84c962213e61aabed1
Denis-san <denis.nt.nc@gmail.com>
2023-01-16 16:29:47 -0300

    email already exists validation

--- src/main/java/com/dsan/springrestapicourse/repositories/ClientRepository.java
+++ src/main/java/com/dsan/springrestapicourse/repositories/ClientRepository.java
@@ -1,11 +1,16 @@
 package com.dsan.springrestapicourse.repositories;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.dsan.springrestapicourse.domain.Client;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Integer>{
+	
+	@Transactional(readOnly = true)
+	Client findByEmail(String email);
 
 }
--- src/main/java/com/dsan/springrestapicourse/services/validation/ClientInsertValidator.java
+++ src/main/java/com/dsan/springrestapicourse/services/validation/ClientInsertValidator.java
@@ -6,12 +6,18 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.dsan.springrestapicourse.domain.enums.ClientType;
 import com.dsan.springrestapicourse.dto.NewClientDTO;
+import com.dsan.springrestapicourse.repositories.ClientRepository;
 import com.dsan.springrestapicourse.resources.exceptions.FieldMessage;
 import com.dsan.springrestapicourse.services.validation.utils.BR;
 
 public class ClientInsertValidator implements ConstraintValidator<ClientInsert, NewClientDTO> {
+	
+	@Autowired
+	private ClientRepository repository;
 
 	@Override
 	public void initialize(ClientInsert ann) {
@@ -28,6 +34,12 @@ public class ClientInsertValidator implements ConstraintValidator<ClientInsert, 
 		if (objDto.getClientType().equals(ClientType.LEGAL_PERSON.getCode()) && !BR.isValidCNPJ(objDto.getCpfOrCnpj())) {
 			list.add(new FieldMessage("cpfOrCnpj", "Invalid CNPJ"));
 		}
+		
+		
+		if(repository.findByEmail(objDto.getEmail()) != null) {
+			list.add(new FieldMessage("email", "Email already exists"));
+		}
+		
 
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
